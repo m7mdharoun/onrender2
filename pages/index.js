e298364a783e73a445c8ca50ecba2beeedb4d266
m7mdharoun <34725185+m7mdharoun@users.noreply.github.com>
2023-05-16 21:31:33 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 const Index = () => (
   <div>
     Hello World.{' '}
-    <Link href="/Pastes.io">
+    <Link href="https://google.com">
       <a>About</a>
     </Link>
   </div>
